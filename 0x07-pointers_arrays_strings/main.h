1b9ddd4071377341a4bfe907879976a8477f328c
eyosidays <eyosiyas.chacha@gmail.com>
2023-03-06 05:42:11 -0800

    Update

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN H
+
+#define MAIN H
+
+char memset(char *s, char b, unsigned int n);
+
+ant_putchar(char c);
+
+char_memcpy(chap "dest, char *src, unsigned int n);
+
+char strchr(char *s, char c);
+
+unsigned int _strspn(char *s, char *accept); char strpbrk (char *s, char *accept);
+
+char_strstr(char *haystack, char *needle); void print_chessboard (char (*a)[8]);
+
+void print_diagsums (int *a, int size);
+
+void set_string(char **s, char *to);
+
+#endif
+
+
